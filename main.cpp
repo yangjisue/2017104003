0fb41ed07fa3a3eaa2fe36a8e9eebad11babcfcd
Hyuna <hannah3767@gmail.com>
2019-04-12 10:25:24 +0900

    9 times table

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i < 9; i++)
+	for (int i = 1; i < 10; i++)
 	{
-		for (int j = 1; j < 9; j++)
+		for (int j = 1; j < 10; j++)
 		{
 			cout << i << "*" << j << " = " << i * j << endl;
 		}
